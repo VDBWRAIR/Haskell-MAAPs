ff394daa7ce10d0760a0c086af25fffeab87db01
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2016-04-15 16:18:32 -0400

    commandline parsing with optparse-generic

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,46 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE StandaloneDeriving     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds         #-}  -- these two needed for help output
+{-# LANGUAGE TypeOperators     #-}
 module Main where
 
-import Lib
+import Lib  (process, RowType(..))
+import Options.Generic
+import Options.Applicative.Types
+-- type FilePath = String
+data Seperator = Tab | Comma
+  deriving (Show, Eq, Generic)
+-- , geneSource :: Maybe GenBankSource <?> "Genbank ID, Genbank file, or csv file"
+      
+deriving instance Read Seperator
+instance ParseField Seperator
+      
+deriving instance Read RowType
+instance ParseField RowType
+data Options = Options {   filter :: [RowType] <?> "What show -- Insert, etc.."
+                         , sep :: First Seperator <?> "Comma or Tab output"
+                         , fasta :: FilePath <?> "Input aligned fasta file"
+                         , align :: Bool <?> "Align the sequence first?"}
+             deriving (Generic, Show)
+
+instance ParseRecord Options
 
 main :: IO ()
-main = someFunc
+main = do
+  x <- getRecord "Running Program"
+  print (x :: Options)
+
+  
+--data GenBankSource = GBFile FilePath | CSVFile FilePath | GBID String
+--  deriving (Show, Generic, ParseRecord)
+
+--instance ParseField GenBankSource where
+--      parseField m = fmap GenBankSource (parseField m)
+      
+  --process (x :: Options)
+
+--instance Read Seperator
+--instance ParseField Seperator where
+--     --parseField m s = fmap (\x -> if (x == "tab") then Tab else Comma) (parseField m s)
+--     parseField (Just m) (Just s) = NilP $ Just (if (m == "tab") then Tab else Comma)
